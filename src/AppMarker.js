c611644c6dccc6af40b7712fa368214e1e2015cc
Wienardo Dana Pramita <wienardo.dana@weekendinc.com>
2021-02-25 16:47:03 +0700

    s

--- src/AppMarker.js
+++ src/AppMarker.js
@@ -10,9 +10,9 @@ function AppMarker() {
                 {/* <!-- create your content here. just a box for now --> */}
                 <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
                 <a-assets>
-                    <a-asset-item id="tree" src="../assets/PikachuThick_tail.gltf"></a-asset-item>
+                    <a-asset-item id="tree" src="../assets/scene.gltf"></a-asset-item>
                 </a-assets>
-                <a-entity gltf-model="url(../assets/PikachuThick_tail.gltf)"></a-entity>
+                <a-entity gltf-model="url(../assets/scene.gltf)"></a-entity>
                 {/* <!-- define a camera which will move according to the marker position --> */}
                 <a-marker-camera preset='hiro'></a-marker-camera>
             </a-scene>
