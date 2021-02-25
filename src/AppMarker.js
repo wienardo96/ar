27f09c15f38cdb4843eb180b65c3380f22a97248
Wienardo Dana Pramita <wienardo.dana@weekendinc.com>
2021-02-25 15:07:46 +0700

    a

--- src/AppMarker.js
+++ src/AppMarker.js
@@ -7,26 +7,12 @@ function AppMarker() {
                 <div><img src="../assets/favicon.ico" alt="test" />Loading, please wait...</div>
             </div>
             <a-scene embedded arjs>
-                {/* <!-- we use cors proxy to avoid cross-origin problems --> */}
-                {/* <a-nft
-                    type="nft"
-                    url="../assets/testing"
-                    smooth="true"
-                    smoothCount="10"
-                    smoothTolerance=".01"
-                    smoothThreshold="5"
-                >
-                    <a-entity
-                        gltf-model="../assets/PikachuThick_tail.gltf"
-                    // scale="5 5 5"
-                    // position="50 150 0"
-                    ></a-entity>
-                </a-nft> */}
                 <a-assets>
                     <a-asset-item id="pika" src="../assets/PikachuThick_tail.gltf"></a-asset-item>
                 </a-assets>
-                <a-entity gltf-model="##pika"></a-entity>
-                <a-marker-camera type='pattern' url='../assets/pattern-marker.patt'></a-marker-camera>
+                <a-marker-camera type='pattern' url='../assets/pattern-marker.patt'>
+                    <a-entity gltf-model="##pika"></a-entity>
+                </a-marker-camera>
                 <a-entity camera></a-entity>
             </a-scene>
         </div>
