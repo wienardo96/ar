ad4a6505c860e52d11720a261ac3d16fe1d80789
Wienardo Dana Pramita <wienardo.dana@weekendinc.com>
2021-02-25 15:14:35 +0700

    test

--- src/App.js
+++ src/App.js
@@ -10,23 +10,26 @@ function App() {
         vr-mode-ui="enabled: false;"
         renderer="logarithmicDepthBuffer: true;"
         embedded
-        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
-      >
-        {/* <!-- we use cors proxy to avoid cross-origin problems --> */}
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        {/* <!-- a-nft is the anchor that defines an Image Tracking entity -->
+      <!-- on 'url' use the path to the Image Descriptors created before. -->
+      <!-- the path should end with the name without the extension e.g. if file is trex.fset' the path should end with trex --> */}
         <a-nft
           type="nft"
           url="../assets/testing"
           smooth="true"
           smoothCount="10"
           smoothTolerance=".01"
-          smoothThreshold="5"
-        >
+          smoothThreshold="5">
+          {/* <!-- as a child of the a-nft entity, you can define the content to show. here's a GLTF model entity --> */}
           <a-entity
-            gltf-model="../assets/PikachuThick_tail.gltf"
-          // scale="5 5 5"
-          // position="50 150 0"
-          ></a-entity>
+            gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
+            scale="5 5 5"
+            position="100 100 0"
+          >
+          </a-entity>
         </a-nft>
+        {/* <!-- static camera that moves according to the device movemenents --> */}
         <a-entity camera></a-entity>
       </a-scene>
     </div>
