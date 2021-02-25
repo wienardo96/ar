8499b4c6a6283233ed5808e0e5e0fe00197fb6ae
Wienardo Dana Pramita <wienardo.dana@weekendinc.com>
2021-02-25 15:11:13 +0700

    a

--- src/AppMarker.js
+++ src/AppMarker.js
@@ -7,11 +7,8 @@ function AppMarker() {
                 <div><img src="../assets/favicon.ico" alt="test" />Loading, please wait...</div>
             </div>
             <a-scene embedded arjs>
-                <a-assets>
-                    <a-asset-item id="pika" src="../assets/PikachuThick_tail.gltf"></a-asset-item>
-                </a-assets>
                 <a-marker-camera type='pattern' url='../assets/pattern-marker.patt'>
-                    <a-entity gltf-model="##pika"></a-entity>
+                    <a-entity gltf-model="url(../assets/PikachuThick_tail.gltf)"></a-entity>
                 </a-marker-camera>
                 <a-entity camera></a-entity>
             </a-scene>
