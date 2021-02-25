030123556e156fa60a7eb0488b74c9b0d699a578
Wienardo Dana Pramita <wienardo.dana@weekendinc.com>
2021-02-25 15:30:22 +0700

    a

--- src/AppMarker.js
+++ src/AppMarker.js
@@ -9,6 +9,10 @@ function AppMarker() {
             <a-scene embedded arjs='sourceType: webcam;'>
                 {/* <!-- create your content here. just a box for now --> */}
                 <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+                <a-assets>
+                    <a-asset-item id="tree" src="../assets/PikachuThick_tail.gltf"></a-asset-item>
+                </a-assets>
+                <a-entity gltf-model="##tree"></a-entity>
                 {/* <!-- define a camera which will move according to the marker position --> */}
                 <a-marker-camera preset='hiro'></a-marker-camera>
             </a-scene>
